736e821e49afb777327803f30f386df738ca5eee
Duynguyen162 <duynguyen162204@gmail.com>
2025-08-26 00:50:21 +0700

    chinh lai quan li dich vu

--- app/src/main/java/com/example/freelancer_connect/model/Category.java
+++ app/src/main/java/com/example/freelancer_connect/model/Category.java
@@ -2,13 +2,32 @@ package com.example.freelancer_connect.model;
 
 public class Category {
     private String name;
-    private int iconResId;
-
-    public Category(String name, int iconResId) {
-        this.name = name;
-        this.iconResId = iconResId;
+    private String description;
+    private String icon; // URL ảnh
+    private String id;
+    public Category() {
+        // Bắt buộc có constructor rỗng cho Firestore
     }
 
-    public String getName() { return name; }
-    public int getIconResId() { return iconResId; }
+    public Category(String name, String description, String icon) {
+        this.name = name;
+        this.description = description;
+        this.icon = icon;
+        this.id = id;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
 }
